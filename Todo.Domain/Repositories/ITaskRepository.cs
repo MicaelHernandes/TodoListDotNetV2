8885ef2de0029e479c564c810048b782188cd4fe
MicaelHernandes <micaelmacedo100@outlook.com>
2025-07-15 18:44:09 -0300

    refactor: removendo dois metodos inutilizados

--- Todo.Domain/Repositories/ITaskRepository.cs
+++ Todo.Domain/Repositories/ITaskRepository.cs
@@ -2,9 +2,7 @@ namespace Todo.Domain.Repositories;
 
 public interface ITaskRepository
 {
-    public Task<Domain.Entities.Task> Add(Task task);
-    public Task<Domain.Entities.Task> Update(Task task);
-    public Task<Domain.Entities.Task> Delete(Task task);
-    public Task<Domain.Entities.Task> FindByEmail(string email);
-    public Task<Domain.Entities.Task> FindByUsername(string username);
+    public Task<Domain.Entities.Task> Add(Domain.Entities.Task task);
+    public Task<Domain.Entities.Task> Update(Domain.Entities.Task task);
+    public Task<bool> Delete(Domain.Entities.Task task);
 }
