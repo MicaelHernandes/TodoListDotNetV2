466855d12a47998895ae3d274302d195a0e80416
MicaelHernandes <micaelmacedo100@outlook.com>
2025-07-15 18:38:43 -0300

    feat: criando contratos para repositorios de task e de usuarios

---
+++ Todo.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Repositories;
+
+public interface IUserRepository
+{
+    public Task<User> Create(User user);
+    public Task<User> Update(User user);
+    public Task<bool> Delete(User user);
+    public Task<User> FindByEmail(string email);
+    public Task<User> FindByUsername(string username);
+}
