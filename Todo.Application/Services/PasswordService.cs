d6d073d60b02eeb4beae383bbe545179e57cf4f3
MicaelHernandes <micaelmacedo100@outlook.com>
2025-07-16 19:26:14 -0300

    feat: criando classe para realizar a criptografia e checagem de senha

---
+++ Todo.Application/Services/PasswordService.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Todo.Application.Services;
+
+public class PasswordService : IPasswordService
+{
+    private readonly PasswordHasher<object> _hasher = new();
+
+    public string Hash(string password)
+    {
+        return _hasher.HashPassword(null, password);
+    }
+
+    public bool Verify(string hashedPassword, string providedPassword)
+    {
+        var result = _hasher.VerifyHashedPassword(null, hashedPassword, providedPassword);
+        return result == PasswordVerificationResult.Success;
+    }
+}
