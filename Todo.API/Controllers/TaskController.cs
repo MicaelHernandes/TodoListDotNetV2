0b0f385d2aa20c909f5c9c9a2649e2134a04a6f3
MicaelHernandes <micaelmacedo100@outlook.com>
2025-07-20 18:46:26 -0300

    feat: criando metodo exibição de dados de uma task

--- Todo.API/Controllers/TaskController.cs
+++ Todo.API/Controllers/TaskController.cs
@@ -7,6 +7,7 @@ using Todo.Application.DTOs.Request.Task;
 using Todo.Application.DTOs.Request.User;
 using Todo.Application.DTOs.Response;
 using Todo.Application.DTOs.Response.Task;
+using Todo.Application.Exceptions;
 using Todo.Application.Exceptions.Task;
 using Todo.Application.UseCases.Task;
 
@@ -56,6 +57,32 @@ namespace Todo.API.Controllers
             catch (CreateTaskInvalidParametersException ex)
             {
                 return StatusCode(StatusCodes.Status400BadRequest, new ApiResponse<string>(ex.Message));
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ApiResponse<string>(e.Message));
+            }
+        }
+
+        [HttpGet("{idTask:int}")]
+        public async Task<IActionResult> Get(
+            [FromServices] GetTaskUseCase useCase,
+            [FromRoute] int idTask
+            )
+        {
+            try
+            {
+                var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+                var response = await useCase.Execute(idTask, userId);
+                return StatusCode(StatusCodes.Status200OK,new ApiResponse<ListTasksResponse>(response));
+            }
+            catch (NotFoundException ex)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, new ApiResponse<string>(ex.Message));
+            }
+            catch (ForbiddenRequestException ex)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, new ApiResponse<string>(ex.Message));
             }
             catch (Exception e)
             {
