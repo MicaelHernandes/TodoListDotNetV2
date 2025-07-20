5d35f2e4b58e48abdadc87764fecb643b5467bbe
MicaelHernandes <micaelmacedo100@outlook.com>
2025-07-20 11:36:03 -0300

    feat: criando metodo de login na api exception handler

--- Todo.API/Controllers/UserController.cs
+++ Todo.API/Controllers/UserController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Todo.Application.DTOs.Request.User;
 using Todo.Application.DTOs.Response;
+using Todo.Application.DTOs.Response.User;
 using Todo.Application.Exceptions.User;
 using Todo.Application.UseCases.User;
 
@@ -30,6 +31,28 @@ namespace Todo.API.Controllers
             catch (Exception e)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, new ApiResponse<string>(e.Message));
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Auth(
+            [FromServices] UserAuthUseCase useCase,
+            [FromBody] UserAuthRequest request
+        )
+        {
+            try
+            {
+                var result = await useCase.Execute(request);
+                return StatusCode(StatusCodes.Status200OK, new ApiResponse<UserAuthResponse>(result));
+            }
+            catch (AuthUserInvalidCredentialsException ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new ApiResponse<AuthUserInvalidCredentialsException>(ex.Message));
+            }
+            catch (Exception c)
+            {
+                Console.WriteLine($"Erro ao autenticar usuario: {c.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, new ApiResponse<string>(c.Message));
             }
         }
     }
