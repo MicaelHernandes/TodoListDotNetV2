71b0961c513a5f4d8ea2d3d0a7806d0931ef0808
MicaelHernandes <micaelmacedo100@outlook.com>
2025-07-20 11:30:36 -0300

    feat: criando request e response para a autenticação de usuario

--- Todo.Application/DTOs/Request/User/CreateUserRequest.cs
+++ Todo.Application/DTOs/Request/User/CreateUserRequest.cs
@@ -5,10 +5,14 @@ namespace Todo.Application.DTOs.Request.User;
 public class CreateUserRequest
 {
     [Required(ErrorMessage = "O campo nome não pode ser vazio")]
+    [MinLength(3, ErrorMessage = "O campo nome deve conter pelo menos 3 caracteres!")]
+    [MaxLength(100, ErrorMessage = "O campo nome deve conter no maximo 100 caracteres!")]
     public string Name { get; set; }
-    [Required(ErrorMessage = "O campo email é obrigatório!")]
-    [EmailAddress(ErrorMessage = "É necessário ser um email valido!")]
+    [Required(ErrorMessage = "O campo e-mail é obrigatorio!")]
+    [EmailAddress(ErrorMessage = "O campo deve ser um e-mail valido!")]
     public string Email { get; set; }
-    [Required(ErrorMessage = "O campo password obrigatório!")]
+    [Required(ErrorMessage = "O campo password é obrigatorio!")]
+    [MinLength(6, ErrorMessage = "A senha deve conter pelo menos 6 caracteres!")]
+    [MaxLength(256, ErrorMessage = "A senha deve conter pelo menos 256 caracteres!")]
     public string Password { get; set; }
 }
